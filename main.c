05f58fe9da8c94bcb83d8898340c7099604c71e2
Gabriel Fonseca <fonsecacrz7@gmail.com>
2021-03-25 14:14:36 -0300

    fix: countdown implementation

--- main.c
+++ main.c
@@ -1,69 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <time.h>
-
-/* necessary to use getch(), don't need to press enter */
-/* #include <ncurses.h> */
+#include <errno.h>
+/* #include <getopt.h> */
 
 #define HELP "USAGE: cpmdrtimer \
--t <int> \
+-w <int> \
 -r <int> \
 \n\n\
 OPTIONS: \n \
--t   work time \n \
--r   rest time \n \
--h   this help message \n"
+-w, --work   work time \n \
+-r, --rest   rest time \n \
+-h, --help   this help message \n"
+
+/* TODO: to implement long options */
+/* static struct option long_options[] = { */
+   /* {"work", no_argument, 0, "w"}, */
+   /* {"rest", no_argument, 0, "r"}, */
+   /* {"help", no_argument, 0, "h"}, */
+   /* {0, 0, 0, 0} */
+/* }; */
 
 void help();
-void timer(int time);
-void rest();
+void timer(int work_val);
+void rest(int rest_val);
+void fail(int line, char *msg);
 
-int main(int argc, char **argv) {
+int
+main(int argc, char **argv) {
    int opt;
 
-   /* initscr(); */
+   if (argc == 1) printf(HELP);
 
-   while ((opt = getopt(argc, argv, "t:r:h")) != -1 ) {
+   while ((opt = getopt(argc, argv, "w:r:h")) != -1 ) {
       switch (opt) {
-         case 't': 
+         case 'w': 
             timer(atoi(optarg));
             break;
          case 'r':
-            rest(optarg);
+            rest(atoi(optarg));
             break;
          case 'h':
             help();
             break;
          default:
             help();
-            exit(1);
+            exit(EXIT_FAILURE);
             break;
       }
    }
 
-   return 0;
+   return EXIT_SUCCESS;
 }
 
 void help() {
    printf(HELP);
 }
 
-void timer(int time) {
+void 
+timer(int work_val) {
    char key;
-   unsigned int cur = 0, end = time * 1000 * 60 * 60;
-   clock_t start = clock();
+   int end, out;
+   time_t start, tmp;
+   struct tm *start_localtime, *cur;
 
-   while(cur < end) {
-      clock_t diff = clock() - start;
-      cur = (diff * 1000 * 60) / CLOCKS_PER_SEC; 
+   if ((start = time(NULL)) < 0) fail(60, "error to start timer");
+
+   if ((start_localtime = localtime(&start)) == NULL) {
+         fail(62, "error to convert start of type time_t to localtime value");
+   }
+
+   end = start_localtime->tm_min + work_val;
+
+   while(start_localtime->tm_min < end) {
+      if ((tmp = time(NULL)) < 0) fail(69, "error to start timer");
+
+      if ((cur = localtime(&tmp)) == NULL) {
+         fail(71, "error to convert tmp of type time_t to localtime value");
+      }
+
+      out = end - (start_localtime->tm_min + (cur->tm_min - start_localtime->tm_min)); 
 
       scanf("%c", &key);
-      if (key == 's') printf("%d\n", ((time * cur) / end));
+      if (key == 's') printf("%d\n", (
+               out != 1 ?
+               out :
+               60 - localtime(&tmp)->tm_sec
+               ));
    };
 
-   printf("END");
+   printf("END\n");
 }
 
-void rest(int rest) {
+void
+rest(int rest_val) {
+   printf("rest time: %d\n", rest_val);
+}
+
+void
+fail(int line, char *msg) {
+   fprintf(stderr, "cpmrtimer:%d: %s [%s]", line, msg, strerror(errno));
+   exit(EXIT_FAILURE);
 }
